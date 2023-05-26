5569f7d140e458c998ea3b24dfdae63a633255df
Erwan DAYOT <pro.e.dayot@gmail.com>
2023-05-26 10:35:22 +0200

    ajout de _Elections.accdb (vielle version)

--- bdd_connect.py
+++ bdd_connect.py
@@ -1,11 +1,23 @@
-
-
 import pyodbc
 
 # Connect to _Elections.accdb
-conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=_Elections.accdb;')
+path = "C:\\Users\\erwan\\Documents\\Dev\\bdd-election\\_Elections.accdb"
+
+# Establish the connection
+conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=' + path + ';')
+
+# Create a cursor object
 cursor = conn.cursor()
 
-cursor.execute("SELECT * FROM Elections")
-for row in cursor.fetchall():
+# Execute a query
+cursor.execute("SELECT * FROM Region")
+
+# Fetch all rows
+rows = cursor.fetchall()
+
+# Process the rows
+for row in rows:
     print(row)
+
+# Close the connection
+conn.close()
